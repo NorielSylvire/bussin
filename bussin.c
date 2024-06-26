8c67e00ff7812959f938f668f17c37eb463dace5
NorielSylvire <fl4v1u5@yandex.com>
2024-06-26 20:07:10 +0200

    bro rick ain't cappin rn stfu

---
+++ bussin.c
@@ -0,0 +1,22 @@
+#include "bussin.h"
+
+homeboi chief(nah)
+{
+	homeboi rick be cap rn
+
+	yknowwhat("give me one number boi: ") rn
+	tellme("%d", &rick) rn
+	sussin (rick is cap or rick < cap)
+	{
+		yknowwhat("wrong fucking answer\n") rn
+		rick be 24 rn
+	}
+	yappin sussin (rick > 100)
+		stfu
+	bussin (no cap n rick aint cap)
+	{
+		yknowwhat("bro rick ain't cap\n") rn
+		rick cappin rn
+	}
+	yeet no cap rn
+}
---
+++ bussin.h
@@ -0,0 +1,22 @@
+#define chief main
+#define homeboi int
+#define rn ;
+#define fr ++
+#define sussin if
+#define bussin while
+#define cappin --
+#define cap 0
+#define no !
+#define yeet return
+#define tellme scanf
+#define yknowwhat printf
+#define nah void
+#define be =
+#define is ==
+#define aint !=
+#define n &&
+#define or ||
+#define yappin else
+#define stfu return 1;
+
+#include <stdio.h>
